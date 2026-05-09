b7538a8ad0691e49132c718b5eb947ff43d2b416
irem <irembekdemir@hacettepe.edu.tr>
2026-05-09 17:06:57 +0300

    Create StorageModel.cs

---
+++ CarRentalSystem/Models/StorageModel.cs
@@ -0,0 +1,8 @@
+namespace CarRentalSystem.Models
+{
+    public class StorageModel
+    {
+        public List<Car> Cars { get; set; }
+        public List<Reservation> Reservations { get; set; }
+    }
+}
